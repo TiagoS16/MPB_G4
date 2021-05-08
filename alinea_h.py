7a98dd5868b7f32d4cf9aab31e5fe8e0144f06df
Tiago <16.tiagosilva@gmail.com>
2021-05-09 00:14:58 +0100

    ...

--- alinea_h.py
+++ alinea_h.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import numpy as np
+import numdifftools as nd
 import matplotlib.pyplot as plt
-import sympy
+import sympy as sym
 from scipy.optimize import fsolve
 from sympy import *
 
 
 #sympy symbols
-X, S, A, P = symbols('X S A P')
+X, S, A, P = symbols('X,S,A,P')#,umax1,umax2,umax3,Ks1,Ks2,Ks3,Fin,Fout,S0,k1,k2,k3,k4,k11
 
 #Reactions
+# u1 = umax1 * (S / (Ks1 + S))
+# u2 = umax2 * (S / (Ks2 + S))
+# u3 = umax3 * (A / (Ks3 + A))
 u1 = 0.25 * (S / (0.3 + S))
 u2 = 0.55 * (S / (0.3 + S))
 u3 = 0.25 * (A / (0.4 + A))
@@ -48,92 +52,90 @@ todas= [difX, difS, difA, difP]
 
 
 def derX(t=False):
-    dX = [sympy.diff(difX, X), sympy.diff(difX, S), sympy.diff(difX, A), sympy.diff(difX, P)]
+    dX = [sym.diff(difX, X), sym.diff(difX, S), sym.diff(difX, A), sym.diff(difX, P)]
     if t == True:
         print('Derivada X X:')
-        print(sympy.diff(difX, X))
+        print(sym.diff(difX, X))
         print()
         print('Derivada X S:')
-        print(sympy.diff(difX, S))
+        print(sym.diff(difX, S))
         print()
         print('Derivada X A:')
-        print(sympy.diff(difX, A))
+        print(sym.diff(difX, A))
         print()
         print('Derivada X P:')
-        print(sympy.diff(difX, P))
+        print(sym.diff(difX, P))
     return dX
 
 
 def derS(t=False):
-    dS = [sympy.diff(difS, X), sympy.diff(difS, S), sympy.diff(difS, A), sympy.diff(difS, P)]
+    dS = [sym.diff(difS, X), sym.diff(difS, S), sym.diff(difS, A), sym.diff(difS, P)]
     if t == True:
         print('Derivada S X:')
-        print(sympy.diff(difS, X))
+        print(sym.diff(difS, X))
         print()
         print('Derivada S S:')
-        print(sympy.diff(difS, S))
+        print(sym.diff(difS, S))
         print()
         print('Derivada S A:')
-        print(sympy.diff(difS, A))
+        print(sym.diff(difS, A))
         print()
         print('Derivada S P:')
-        print(sympy.diff(difS, P))
+        print(sym.diff(difS, P))
     return dS
 
 
 def derA(t=False):
-    dA = [sympy.diff(difA, X), sympy.diff(difA, S), sympy.diff(difA, A), sympy.diff(difA, P)]
+    dA = [sym.diff(difA, X), sym.diff(difA, S), sym.diff(difA, A), sym.diff(difA, P)]
     if t == True:
         print('Derivada A X:')
-        print(sympy.diff(difA, X))
+        print(sym.diff(difA, X))
         print()
         print('Derivada A S:')
-        print(sympy.diff(difA, S))
+        print(sym.diff(difA, S))
         print()
         print('Derivada A A:')
-        print(sympy.diff(difA, A))
+        print(sym.diff(difA, A))
         print()
         print('Derivada A P:')
-        print(sympy.diff(difA, P))
+        print(sym.diff(difA, P))
     return dA
 
 
 def derP(t=False):
-    dP = [sympy.diff(difP, X), sympy.diff(difP, S), sympy.diff(difP, A), sympy.diff(difP, P)]
+    dP = [sym.diff(difP, X), sym.diff(difP, S), sym.diff(difP, A), sym.diff(difP, P)]
     if t == True:
         print('Derivada P X:')
-        print(sympy.diff(difP, X))
+        print(sym.diff(difP, X))
         print()
         print('Derivada P S:')
-        print(sympy.diff(difP, S))
+        print(sym.diff(difP, S))
         print()
         print('Derivada P A:')
-        print(sympy.diff(difP, A))
+        print(sym.diff(difP, A))
         print()
         print('Derivada P P:')
-        print(sympy.diff(difP, P))
+        print(sym.diff(difP, P))
     return dP
 
 derX()
-print('='*66)
+#print('='*66)
 derS()
-print('='*66)
+#print('='*66)
 derA()
-print('='*66)
+#print('='*66)
 derP()
 
 
-def fun(X):
-    S= 0
-    A= 0
-    P= 0
+def fun(x):
+    X, S, A, P = x
     # Reactions
     u1 = 0.25 * (S / (0.3 + S))
     u2 = 0.55 * (S / (0.3 + S))
     u3 = 0.25 * (A / (0.4 + A))
 
     # Initial conditions
-    S0 = 0  # g/L
+    S0 = 12 # g/L
 
     # Parameters
     k1 = 4.412
@@ -143,12 +145,68 @@ def fun(X):
     k11 = 13.21
     Fin = 0.7
 
-    return [(u1 * X[0] + u2 * X[0] + u3 * X[0] - Fin * X[0]),
-            (- k1 * u1 * X[1] - k2 * u2 * X[1] + Fin * S0 - Fin * S),
-            (k3 * u2 * X[2] - k4 * u3 * X[2] - Fin * A),
-            (k11 * u1 * X[3] - Fin * P)]
+    return [(u1 * X + u2 * X + u3 * X - Fin * X),
+            (- k1 * u1 * X - k2 * u2 * X + Fin * S0 - Fin * S),
+            (k3 * u2 * X - k4 * u3 * X - Fin * A),
+            (k11 * u1 * X - Fin * P)]
 
 
-root = fsolve(fun, [1, 1, 1, 1])
+root = fsolve(fun, [0, 12, 0, 0])
 print(root)
 print(np.isclose(fun(root), [0.0, 0.0, 0.0, 0.0]))
+print()
+root1 = fsolve(fun, [7, 12, 0, 0])
+print(root1)
+print(np.isclose(fun(root1), [0.0, 0.0, 0.0, 0.0]))
+print()
+
+
+# difX= u1 * X + u2 * X + u3 * X - Fin * X
+# difS= - k1 * u1 * X - k2 * u2 * X + Fin * S0 - Fin * S
+# difA= k3 * u2 * X - k4 * u3 * X - Fin * A
+# difP= k11 * u1 * X - Fin * P
+# difV= Fin - Fout
+
+
+def Jacobian(v_str, f_list):
+    global J
+    vars = sym.symbols(v_str)
+    f = sym.sympify(f_list)
+    J = sym.zeros(len(f),len(vars))
+    for i, fi in enumerate(f):
+        for j, s in enumerate(vars):
+            J[i,j] = sym.diff(fi, s)
+    J = np.array(J).tolist()
+    return J
+Jacobian('X G A P', ['u1*X + u2*X + u3*X - Fin*X',' - k1*u1*X - k2*u2*X + Fin*S0 - G*Fin', 'k3*u2* X - k4*u3*X - Fin*A', 'k11*u1*X - Fin*P'])
+
+#ponto 1
+#X1, G1, A1, P1 = 0, 12, 0, 0
+
+#u1 = 0.2439
+#u2 = 0.53659
+#u3 = 0
+
+JJ = [[0.08049, 0, 0, 0], [-12.9991, -0.7, 0, 0], [4.62, 0, -0.7, 0], [3.22192, 0, 0, -0.7]]
+JJJ = np.array(JJ)
+jjjj = np.linalg.det(JJJ)
+print('O determinante do ponto 1 é {}'.format(jjjj))
+jjjjj = np.trace(JJJ)
+print('O traço do ponto 1 é {}'.format(jjjjj))
+
+
+#ponto 2
+#X2, S2, A2, P2 = -1.37289662*(10**-9), 1.20000000*(10**1), -1.06074905*(10**-8), -6.31708043*(10**-9)
+#u1=0.2439
+#u2=0.5638
+#u3=0.1524
+
+LL = [[0.2611, 0, 0, 0], [-13.6037, -0.7, 0, 0], [3.3538, 0, -0.7, 0], [3.22192, 0, 0, -0.7]]
+LLL = np.array(LL)
+llll = np.linalg.det(LLL)
+print('O determinante do ponto 2 é {}'.format(llll))
+lllll = np.trace(LLL)
+print('O traço do ponto 2 é {}'.format(lllll))
+
+
+#são os dois pontos sela
